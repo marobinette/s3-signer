65adc67d8daef570f853c234e0c6b48e5a645446
Michael Robinette <michaelrobinette@Michaels-MacBook-Pro.local>
2023-08-02 11:08:50 -0400

    working app

--- lib/s3-signer-stack.ts
+++ lib/s3-signer-stack.ts
@@ -1,19 +1,53 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
+import * as cdk from 'aws-cdk-lib';
+
 import { Construct } from 'constructs';
 
 export class S3SignerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'S3SignerQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const signer = new NodejsFunction(this, 'signerHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+			entry: path.join(__dirname, `/../lambdas/signer.js`),
+      handler: 'handler',
+    });
+    const bucketPolicyStatement = new cdk.aws_iam.PolicyStatement({
+			actions: [
+				's3:AbortMultipartUpload',
+				's3:GetBucketLocation',
+				's3:GetObject',
+				's3:ListBucket',
+				's3:ListBucketMultipartUploads',
+				's3:PutObject'
+			],
+			effect: cdk.aws_iam.Effect.ALLOW,
+			resources: [
+        'arn:aws:s3:::reports-service-dev-riskalyze-storage',
+        'arn:aws:s3:::reports-service-dev-riskalyze-storage/*'
+        ],
+		});
+
+    const queuePolicyStatement = new cdk.aws_iam.PolicyStatement({
+      actions: ['sqs:SendMessage'],
+      effect: cdk.aws_iam.Effect.ALLOW,
+      resources: [
+        'arn:aws:sqs:us-east-2:263955829476:html-to-pdf-generator-dev-queue'
+      ]
     });
 
-    const topic = new sns.Topic(this, 'S3SignerTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    signer.role?.attachInlinePolicy(
+			new cdk.aws_iam.Policy(this, 'bucket-policy', {
+				statements: [bucketPolicyStatement],
+			}),
+		);
+    signer.role?.attachInlinePolicy(
+      new cdk.aws_iam.Policy(this, 'queue-policy', {
+        statements: [queuePolicyStatement]
+      }),
+    );
   }
 }
